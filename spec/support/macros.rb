02201c7520942372a8a37d5761af1514ae2e89a0
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-07-25 20:57:15 +0300

    Drop all schemas query improved

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -1,33 +1,13 @@
 def drop_schemas
   connection = ActiveRecord::Base.connection.raw_connection
   schemas = connection.query(%{
-    SELECT string_agg(format('DROP SCHEMA %I CASCADE;', nspname), E'\n')
-    FROM   pg_namespace WHERE nspname != 'public'
-    AND nspname NOT LIKE 'pg_%'
-    AND nspname != 'information_schema';
+    SELECT n.nspname AS "Name",
+      pg_catalog.pg_get_userbyid(n.nspowner) AS "Owner"
+    FROM pg_catalog.pg_namespace n
+    WHERE n.nspname !~ '^pg_'
+    AND n.nspname <> 'information_schema'
+    AND n.nspname != 'public';
   })
-  # %{
-  #  CREATE OR REPLACE FUNCTION drop_all () 
-  #  RETURNS VOID  AS
-  #  $$
-  #  DECLARE rec RECORD; 
-  #  BEGIN
-  #      -- Get all the schemas
-  #       FOR rec IN
-  #       SELECT DISTINCT schemaname
-  #        FROM pg_catalog.pg_tables
-  #        -- You can exclude the schema which you don't want to drop by adding another condition here
-  #        WHERE schemaname NOT LIKE 'pg_%' AND schemaname != 'public' 
-  #        AND schemaname != 'information_schema'
-  #          LOOP
-  #            EXECUTE 'DROP SCHEMA ' || rec.schemaname || ' CASCADE'; 
-  #          END LOOP; 
-  #          RETURN; 
-  #  END;
-  #  $$ LANGUAGE plpgsql;
- 
-  #  SELECT drop_all();
-  # }
   schemas.each do |query|
     connection.query(query.values.first)
   end
